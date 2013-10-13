c492fbd41c3ca055fc7a3e315338e54898ed4c88
richard bluestein <richard@bluestein.com>
2013-10-13 18:10:00 -0500

    added contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -38,6 +38,18 @@ describe "StaticPages" do
       visit '/static_pages/about'
       expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
     end
-
   end
+ describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Contact Us')
+    end
+  end
+
+
 end
